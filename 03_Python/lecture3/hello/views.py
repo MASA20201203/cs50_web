ac7f82855dc6e76aa695b205ba7231656a108bc0
MASA20201203 <koedame032@gmail.com>
2023-12-07 16:23:56 +0900

    テンプレートファイルをレンダリングするよう修正

--- 03_Python/lecture3/hello/views.py
+++ 03_Python/lecture3/hello/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize() }!")
+    return HttpResponse(f"Hello, {name.capitalize()}!")
